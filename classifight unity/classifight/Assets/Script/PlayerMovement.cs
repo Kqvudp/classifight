20be5e9b42ba062dba5bde8ba629e7dda73917d2
Silas <datbinhdang@gmail.com>
2023-04-14 00:06:09 +0700

    Sửa lỗi
    Lỗi bám tường, lỗi đứng im không đánh được

--- classifight unity/classifight/Assets/Script/PlayerMovement.cs
+++ classifight unity/classifight/Assets/Script/PlayerMovement.cs
@@ -7,17 +7,22 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rg;
     private SpriteRenderer sprite;
     private Animator anim;
+    private BoxCollider2D coll;
+    [SerializeField] private LayerMask jumpableGround;
 
-    private float dirX = 0f; 
+    private float dirX = 0f;
     private float MoveSpeed = 7f;
     private float JumpForce = 14f;
+    private bool checkJump = false;
+    private bool checkAttack = false;
     private MovementState state;
-    private enum MovementState { idle, running, jumping, falling, attacking, jmpattacking }
+    private enum MovementState { idle, running, jumping, attacking, jmpattacking }
     // Start is called before the first frame update
     void Start()
     {
         rg = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
     }
 
@@ -26,16 +31,26 @@ public class PlayerMovement : MonoBehaviour
     {
         dirX = Input.GetAxisRaw("Horizontal");
         rg.velocity = new Vector2(dirX * MoveSpeed, rg.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
+        checkAttack = Input.GetKeyDown(KeyCode.J);
+        checkJump = Input.GetKeyDown(KeyCode.Space);
+        if (checkJump && IsGrounded())
         {
             rg.velocity = new Vector2(dirX * rg.velocity.x, JumpForce);
         }
-        Move();
-        if ((Input.GetKeyDown(KeyCode.J)))
+
+        if (checkAttack)
         {
             state = MovementState.attacking;
-            anim.SetInteger("state", (int)state);
-        }    
+        }
+        else if (checkAttack && checkJump)
+        {
+            state = MovementState.jmpattacking;
+        }
+        else
+        {
+            Move();
+        }
+        anim.SetInteger("state", (int)state);
     }
 
     private void Move()
@@ -43,7 +58,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.J))
         {
             state = MovementState.attacking;
-        }    
+        }
         if (dirX > 0f)
         {
             state = MovementState.running;
@@ -59,28 +74,14 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.idle;
         }
 
-        if (rg.velocity.y > .1f)
+        if (rg.velocity.y > .1f || rg.velocity.y < -.1f)
         {
-            if (Input.GetKeyDown(KeyCode.J))
-            {
-                state = MovementState.jmpattacking;
-            }
-            else
-            {
-                state = MovementState.jumping;
-            }
+            state = MovementState.jumping;
         }
-        else if (rg.velocity.y < -.1f)
-        {
-            if (Input.GetKeyDown(KeyCode.J))
-            {
-                state = MovementState.jmpattacking;
-            }
-            else
-            {
-                state = MovementState.falling;
-            }
-        }
-        anim.SetInteger("state", (int)state);
+
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
