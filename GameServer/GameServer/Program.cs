cdf61fa497eac228c87c529bada3928d85d0a06d
NghiaVNM <duongphanhieunghia2003@gmail.com>
2023-04-25 16:09:37 +0700

    update GameServer

--- GameServer/GameServer/Program.cs
+++ GameServer/GameServer/Program.cs
@@ -6,7 +6,8 @@ namespace GameServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Game Server";
+            Console.ReadKey();
         }
     }
 }
