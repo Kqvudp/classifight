68de6dfad822f6c6bdc9527122b7f4eeab6bee33
NghiaVNM <duongphanhieunghia2003@gmail.com>
2023-04-28 11:51:41 +0700

    create client

--- GameServer/GameServer/Server.cs
+++ GameServer/GameServer/Server.cs
@@ -10,15 +10,16 @@ namespace GameServer
     {
         public static int MaxPlayers { get; private set; }
         public static int Port { get; private set; }
-
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
         private static TcpListener tcpListener;
 
-        public void Start(int _maxPlayers, int _port)
+        public static void Start(int _maxPlayers, int _port)
         {
             MaxPlayers = _maxPlayers;
             Port = _port;
 
             Console.WriteLine("Starting server...");
+            IntitializeServerData();
 
             tcpListener = new TcpListener(IPAddress.Any, Port);
             tcpListener.Start();
@@ -31,6 +32,26 @@ namespace GameServer
         {
             TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
             tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+            Console.WriteLine($"Incoming connection from {_client.Client.RemoteEndPoint}...");
+
+            for (int i = 1; i<= MaxPlayers; i++)
+            {
+                if (clients[i].tcp.socket == null)
+                {
+                    clients[i].tcp.Connect(_client);
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{_client.Client.RemoteEndPoint} failed to connect: Server full!");
+        }
+
+        private static void IntitializeServerData()
+        {
+            for (int i = 1; i <= MaxPlayers; i++) 
+            {
+                clients.Add(i, new Client(i));
+            }
         }
     }
 }
