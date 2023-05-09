a8b0a7c226553dd7dfefb72a19fa0b8603b4de09
NghiaVNM <duongphanhieunghia2003@gmail.com>
2023-05-09 09:02:09 +0700

    8

--- GameServer/GameServer/Packet.cs
+++ GameServer/GameServer/Packet.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
 
-namespace GameServerNew
+namespace GameServer
 {
     /// <summary>Sent from server to client.</summary>
     public enum ServerPackets
