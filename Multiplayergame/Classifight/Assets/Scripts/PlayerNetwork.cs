002131210e43bf0ebe453e858fe6e8f2941eb473
ndhoang283 <21520869@gm.uit.edu.vn>
2023-06-02 16:19:11 +0700

    q

--- Multiplayergame/Classifight/Assets/Scripts/PlayerNetwork.cs
+++ Multiplayergame/Classifight/Assets/Scripts/PlayerNetwork.cs
@@ -5,16 +5,56 @@ using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    private Animator animator;
+    public float moveSpeed = 4f;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private MovementState state;
+    private enum MovementState { idle, running, jumping, attacking}
+    private Animator anim;
+    private bool checkJump = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         if (!IsOwner) return;
-        Vector3 moveDir = new Vector3(0, 0, 0);
-        if (Input.GetKey(KeyCode.W)) moveDir.y = +1f;
-        if (Input.GetKey(KeyCode.S)) moveDir.y = -1f;
-        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
-        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
-
-        float moveSpeed = 3f;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        checkJump = (Input.GetKeyDown(KeyCode.W));
+        if (checkJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 7f);
+            state = MovementState.jumping;
+        }
+        else 
+        {
+            if (dirX > 0f)
+            {
+                state = MovementState.running;
+                sprite.flipX = false;
+            }
+            else if (dirX < 0f)
+            {
+                state = MovementState.running;
+                sprite.flipX = true;
+            }
+            else
+            {
+                state = MovementState.idle;
+            }
+            if (Input.GetKey(KeyCode.J))
+            {
+                state = MovementState.attacking;
+            } 
+        }
+        anim.SetInteger("state", (int)state);
     }
+
 }
